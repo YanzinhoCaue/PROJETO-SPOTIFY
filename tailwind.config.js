9f6656b6fea861ebbb4965dd05d712a6050836ac
Yan Caue <coldecmastercrelan@gmail.com>
2023-08-02 15:22:34 -0300

    novos arquivos

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '14': '3.5rem',
+        '125px': '125px',
+      },
+      colors: {
+        'hoverspt': '#18D760',
+        'purple-main': '#2D46B9',
+        'green-main': '#1ED760',
+      },
+      backgroundImage: theme => ({
+        'spotify-theme': "url('./src/images/bursts.svg')",
+        'spotify-theme-mobile': "url('./src/images/bursts-mobile.svg')"
+      }),
+      backgroundSize: {
+        '175%': '175%',
+        '195%': '195%',
+      },
+      backgroundPosition: {
+        'banner': '46% 4%',
+        'banner-mobile': 'top 25% center', 
+      },
+      fontSize: {
+        '9xl': '9rem',
+      },
+    },
   },
   plugins: [],
 }
